48af1b35a124801e822ada09f2c2b4fa09573dff
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-01-31 16:23:02 -0700

    Add tests for fetchData

---
+++ src/helper/apiCall.test.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import * as api from './apiCall'
+
+describe('fetchData', () => {
+  let mockReturnData;
+  let mockURL; 
+
+  beforeEach(() => {
+    mockReturnData = [{}, {}]
+    mockURL = 'http://localhost:3000'
+  })
+  it('should call fetch with the correct paramaters', () => {
+    //setup
+    window.fetch = jest.fn()
+    //execution
+    api.fetchData(mockURL)
+    //expectation
+    expect(window.fetch).toHaveBeenCalledWith(mockURL)
+  })
+
+  it('should return the expected data', async () => {
+    //setup
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+      json: () => Promise.resolve(mockReturnData),
+      ok: true
+    }))
+    //execution
+    const results = await api.fetchData(mockURL)
+    //expectation
+    expect(results).toEqual(mockReturnData)
+  })
+
+  it('throws an error when the fetch fails', async () => {
+    //setup
+    const expectedError = Error('Error fetching, code: 500')
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve ({
+      status: 500,
+      ok: false
+    }))
+    //execution and expectation
+     await expect(api.fetchData(mockURL)).rejects.toEqual(expectedError)
+  })
+})
