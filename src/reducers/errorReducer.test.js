6db3de30d5edf46de1f81931fcb5b0cd02db11fb
Matthew Foxwell <foxwellmw@gmail.com>
2019-02-04 17:31:23 -0700

    Add errorReducer tests

---
+++ src/reducers/errorReducer.test.js
@@ -0,0 +1,25 @@
+import { errorReducer } from './errorReducer'
+import * as actions from '../actions'
+
+describe('errorReducer', () => {
+
+  it('should return initial state', () => {
+    //setup 
+    const expected = ''
+    //execution
+    const result = errorReducer(undefined, {})
+    //expectation
+    expect(result).toEqual(expected)
+  })
+
+  it('should set an error message', () => {
+    //setup
+    const initialState = ''
+    const expected = { message: "Something isn't working" }
+    //execution
+    const result = errorReducer(initialState, actions.setError(expected))
+    //expectation
+    expect(result).toEqual(expected)
+  })
+
+})
