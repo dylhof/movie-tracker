4668764a767ace0425c7b737dc64ffa37729d845
Matthew Foxwell <foxwellmw@gmail.com>
2019-02-05 15:20:48 -0700

    Add store error handling to SignUp

--- src/containers/SignUp/SignUp.js
+++ src/containers/SignUp/SignUp.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { fetchPost } from '../../helper/apiCall';
 import { connect } from 'react-redux';
-import { setCurrentUser } from '../../actions';
+import { setCurrentUser, setError } from '../../actions';
 import PropTypes from 'prop-types'
 
 export class SignUp extends Component {
@@ -38,43 +38,44 @@ export class SignUp extends Component {
       this.props.dispatchSetCurrentUser(this.state.name, response.id)
       this.setState({ name: '', username: '', password: '' })
     } catch (error) {
-      if (error.message === '500') {
-        this.setState({ error: 'This email already exists!' })
-      }
+      this.props.dispatchSetError(error.message)
     }
   }
 
   render() {
+    const { error } = this.props
+    const { name, username, password } = this.state
     return (
       <div className='signup-form-div'>
         <form className='signup-form' onSubmit={this.handleSubmit}>
+          <div className='signup-inner-div'>{error}</div>
           <div className='signup-inner-div'>
             <label className='signup-label' htmlFor='signup-name'>Name</label>
-            <input id='signup-name' className='signup-input' name='name' value={this.state.name} onChange={this.handleChange} />
+            <input id='signup-name' className='signup-input' name='name' value={name} onChange={this.handleChange} />
           </div>
           <div className='signup-inner-div'>
             <label className='signup-label' htmlFor='signup-email'>Email</label>
-            <input id='signup-email' className='signup-input' name='username' value={this.state.username} onChange={this.handleChange} />
+            <input id='signup-email' className='signup-input' name='username' value={username} onChange={this.handleChange} />
           </div>
           <div className='signup-inner-div'>
             <label className='signup-label' htmlFor='signup-password'>Password</label>
-            <input id='signup-password' className='signup-input' name='password' value={this.state.password} onChange={this.handleChange} />
+            <input id='signup-password' className='signup-input' name='password' value={password} onChange={this.handleChange} />
           </div>
           <button className='signup-submit'>Submit</button>
-          <span>{this.state.error}</span>
         </form>
-
       </div>
     )
   }
 }
 
 export const mapDispatchToProps = (dispatch) => ({
-  dispatchSetCurrentUser: (name, userID) => dispatch(setCurrentUser(name, userID))
+  dispatchSetCurrentUser: (name, userID) => dispatch(setCurrentUser(name, userID)),
+  dispatchSetError: (message) => dispatch(setError(message))
 })
 
 export default connect(null, mapDispatchToProps)(SignUp)
 
 SignUp.propTypes = {
-  dispatchSetCurrentUser: PropTypes.func
+  dispatchSetCurrentUser: PropTypes.func,
+  dispatchSetError: PropTypes.func
 }
